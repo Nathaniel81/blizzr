f8057f356aa3426c4b83581fd75e6a9e81e49fc8
Nathaniel81 <nathanield356@gmail.com>
2024-01-16 20:26:25 +0300

    Implement product and category models in StoreApp

--- Backend/StoreApp/models.py
+++ Backend/StoreApp/models.py
@@ -14,7 +14,7 @@ class Product(models.Model):
     name = models.CharField(max_length=200, null=True, blank=True)
     image = models.ImageField(null=True, blank=True, default='no-image.png')
     brand = models.CharField(max_length=200, null=True, blank=True)
-    category = models.ForeignKey(Category, max_length=200, null=True, blank=True)
+    category = models.ForeignKey(Category, max_length=200, null=True, blank=True, on_delete=models.SET_NULL)
     description = models.TextField(null=True, blank=True)
     rating = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
     numReviews = models.IntegerField(null=True, blank=True, default=0)
