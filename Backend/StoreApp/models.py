b232205dfd9d96cad6218eac87790026bc8cc0a5
Nathaniel81 <nathanield356@gmail.com>
2024-01-16 20:20:26 +0300

    Implement product and category models in StoreApp

---
+++ Backend/StoreApp/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    image = models.ImageField(null=True, blank=True, default='no-image.png')
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    # user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    name = models.CharField(max_length=200, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True, default='no-image.png')
+    brand = models.CharField(max_length=200, null=True, blank=True)
+    category = models.ForeignKey(Category, max_length=200, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+    rating = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
+    numReviews = models.IntegerField(null=True, blank=True, default=0)
+    price = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
+    countInStock = models.IntegerField(null=True, blank=True, default=0)
+    createdAt = models.DateTimeField(auto_now_add=True)
+    updatedAt = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
