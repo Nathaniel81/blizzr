e00aac6cb72d2b82f9ca66752e078cbd2baca507
Nathaniel81 <nathanield356@gmail.com>
2024-01-22 14:41:51 +0300

    Updated URL patterns to include the new view

--- backend/store/urls/product_urls.py
+++ backend/store/urls/product_urls.py
@@ -3,4 +3,8 @@ from store.views import product_views as views
 
 urlpatterns = [
 	path('', views.ProductList.as_view(), name='products'),
+	path('detail/<str:pk>/', views.ProductDetail.as_view(), name='product-detail'),
+	path('<str:pk>/review/', views.CreateProductReviewView.as_view(), name="create-review"),
+
+ 
 ]
