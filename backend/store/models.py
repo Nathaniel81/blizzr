c4bc72ffbb920e23a68dc9bf8eb4fb3b73afe238
Nathaniel81 <nathanield356@gmail.com>
2024-01-22 14:40:01 +0300

    Update Product Model

--- backend/store/models.py
+++ backend/store/models.py
@@ -8,11 +8,15 @@ class Category(models.Model):
         return self.name
 
 class Product(models.Model):
-    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    user = models.ForeignKey(User, related_name='products', on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=200, null=True, blank=True)
-    image = models.ImageField(null=True, blank=True, default='no-image.png')
+    main_image = models.ImageField(null=True, blank=True, default='no-image.png')
+    image_1 = models.ImageField(blank=True, null=True, default='no-image.png')
+    image_2 = models.ImageField(blank=True, null=True, default='no-image.png')
+    image_3 = models.ImageField(blank=True, null=True, default='no-image.png')
+    image_4 = models.ImageField(blank=True, null=True, default='no-image.png')
     brand = models.CharField(max_length=200, null=True, blank=True)
-    category = models.ForeignKey(Category, max_length=200, null=True, blank=True, on_delete=models.SET_NULL)
+    category = models.ForeignKey(Category, related_name='products', max_length=200, null=True, blank=True, on_delete=models.SET_NULL)
     description = models.TextField(null=True, blank=True)
     rating = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
     numReviews = models.IntegerField(null=True, blank=True, default=0)
@@ -25,10 +29,10 @@ class Product(models.Model):
         return self.name
 
 class Review(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
+    product = models.ForeignKey(Product, related_name='reviews', on_delete=models.SET_NULL, null=True)
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=200, null=True, blank=True)
-    rating = models.IntegerField(null=True, blank=True, default=0)
+    rating = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True, default=0)
     comment = models.TextField(null=True, blank=True)
     createdAt = models.DateTimeField(auto_now_add=True)
     updatedAt = models.DateTimeField(auto_now=True)
