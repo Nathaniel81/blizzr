5c9f98f7ecaf420514e6917be7658262ca829788
Nathaniel81 <nathanield356@gmail.com>
2024-01-26 21:13:07 +0300

    Implement user register slice and action

---
+++ frontend/src/redux/slices/userSlices/userRegisterSlice.jsx
@@ -0,0 +1,35 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const userRegisterSlice = createSlice({
+  name: 'userRegister',
+  initialState: {
+    loading: false,
+	userInfo: false,
+    error: null,
+  },
+  reducers: {
+    userRegisterRequest: (state) => {
+      state.loading = true;
+    },
+    userRegisterSuccess: (state) => {
+      state.loading = false;
+      state.userInfo = true;
+      state.error = null;
+    },
+    userRegisterFailure: (state, action) => {
+      state.loading = false;
+      state.error = action.payload;
+    },
+	userRegisterReset: (state) => {
+		state.userInfo = false;
+	}
+  },
+});
+
+export const {
+  userRegisterRequest,
+  userRegisterSuccess,
+  userRegisterFailure,
+  userRegisterReset
+} = userRegisterSlice.actions;
+export default userRegisterSlice.reducer;
