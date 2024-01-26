38171459e3040a40c44d2afbda12221192f1ee33
Nathaniel81 <nathanield356@gmail.com>
2024-01-26 21:00:27 +0300

    Implement deliver handler for staff users

---
+++ frontend/src/redux/slices/orderSlices/orderDeliverSlice.jsx
@@ -0,0 +1,41 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+// const initialState = {
+// 	loading: false,
+// 	success: {},
+// 	error: null,
+//   };
+
+const orderDeliverSlice = createSlice({
+  name: 'orderDeliver',
+  initialState: {
+    loading: false,
+    success: null,
+    error: null,
+  },
+  reducers: {
+    orderDeliverRequest: (state) => {
+      state.loading = true;
+    },
+    orderDeliverSuccess: (state, action) => {
+      state.loading = false;
+      state.success = action.payload;
+    },
+    orderDeliverFailure: (state, action) => {
+      state.loading = false;
+      state.error = action.payload;
+    },
+	orderDeliverReset: (state) => {
+		state.success = null;
+		state.error = null;
+	},
+  },
+});
+
+export const {
+	orderDeliverRequest,
+	orderDeliverSuccess,
+	orderDeliverFailure,
+	orderDeliverReset
+} = orderDeliverSlice.actions;
+export default orderDeliverSlice.reducer;
