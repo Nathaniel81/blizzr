a3d87e1d4737936c1d93cae7b12d6190d7e10258
Nathaniel81 <nathanield356@gmail.com>
2024-02-03 17:17:26 +0300

    Collected static files for deployment

--- backend/Blizzr/settings.py
+++ backend/Blizzr/settings.py
@@ -166,7 +166,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = 'staticfiles'
 STATICFILES_DIRS = [
     BASE_DIR / 'static/',
     BASE_DIR / 'frontend/dist/'
