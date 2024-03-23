d142cb3008c263dd2b800778a863c449be7a2f49
Nathaniel81 <nathanield356@gmail.com>
2024-03-23 20:45:26 +0300

    Update requirements.txt

--- backend/Blizzr/settings.py
+++ backend/Blizzr/settings.py
@@ -23,7 +23,7 @@ load_dotenv()
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = "django-insecure-jomg4&dj&fet1o2$rxm9ut9v@&vp*@y7-1@2!$qd!(!fk#44-&"
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
