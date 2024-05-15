95e0e91bfd544a828f2e5904c0cb7cee26292802
Nathaniel81 <nathanield356@gmail.com>
2024-05-15 11:18:10 +0300

    Updated product model's image field to utilize Cloudinary

--- backend/store/models.py
+++ backend/store/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 from accounts.models import User
+from cloudinary.models import CloudinaryField
+
+
 class Category(models.Model):
     name = models.CharField(max_length=200)
-    image = models.ImageField(null=True, blank=True, default='no-image.png')
 
     def __str__(self):
         return self.name
@@ -10,12 +12,13 @@ class Category(models.Model):
 class Product(models.Model):
     user = models.ForeignKey(User, related_name='products', on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=200, null=True, blank=True)
-    main_image = models.ImageField(null=True, blank=True, default='no-image.png')
-    image_1 = models.ImageField(blank=True, null=True, default='no-image.png')
-    image_2 = models.ImageField(blank=True, null=True, default='no-image.png')
-    image_3 = models.ImageField(blank=True, null=True, default='no-image.png')
+    main_image = CloudinaryField('image', null=True, blank=True)
+    image_1 = CloudinaryField('image', null=True, blank=True)
+    image_2 = CloudinaryField('image', null=True, blank=True)
+    image_3 = CloudinaryField('image', null=True, blank=True)
     brand = models.CharField(max_length=200, null=True, blank=True)
-    category = models.ForeignKey(Category, related_name='products', max_length=200, null=True, blank=True, on_delete=models.SET_NULL)
+    category = models.ForeignKey(
+        Category, related_name='products', max_length=200, null=True, blank=True, on_delete=models.SET_NULL)
     description = models.TextField(null=True, blank=True)
     rating = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
     numReviews = models.IntegerField(null=True, blank=True, default=0)
@@ -60,10 +63,11 @@ class OrderItem(models.Model):
     name = models.CharField(max_length=200, null=True, blank=True)
     qty = models.IntegerField(null=True, blank=True, default=0)
     price = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
-    image = models.ImageField(null=True, blank=True, default='no-image.png')
+    image = CloudinaryField('image', null=True, blank=True)
 
     def __str__(self):
         return str(self.name)
+
 class ShippingAddress(models.Model):
     order = models.OneToOneField(Order, on_delete=models.CASCADE, null=True, blank=True)
     address = models.CharField(max_length=200, null=True, blank=True)
